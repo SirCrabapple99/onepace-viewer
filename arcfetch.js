e7cfecb56e6d67d57859bb20de9f10a48cc24f74
SirCrabapple99 <99812880+SirCrabapple99@users.noreply.github.com>
2026-03-09 22:46:34 +0000

    started working on python

--- arcfetch.js
+++ arcfetch.js
@@ -1,24 +1,3 @@
-async function fetchOnePace() {
-    const res = await fetch(`https://whateverorigin.org/get?url=${encodeURIComponent('https://onepace.net/en/watch/')}`)
-    const html = (await res.json()).contents
-    const parser = new DOMParser()
-    // parse the html
-    const doc = parser.parseFromString(html, "text/html")
-    const arcs = []
-    // get all the headers
-    const headers = doc.querySelectorAll("h2")
-    const links = doc.querySelectorAll("a")
-    headers.forEach(h2 => {
-        // grab arc names
-        const arcName = h2.textContent.trim()
-        arcs.push({
-            arc: arcName,
-        })
-    })
+async function parseArcsJSON() {
 
-    alert(JSON.stringify(arcs))
 }
-
-fetchOnePace().then(arcs => {
-        alert(JSON.stringify(arcs))
-    })
