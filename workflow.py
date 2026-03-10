549491274a4bc513d4749009d40ff4d7f16dfbaa
SirCrabapple99 <99812880+SirCrabapple99@users.noreply.github.com>
2026-03-10 01:47:04 +0000

    pixeldrain api

--- workflow.py
+++ workflow.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse
 import json
-import os
+import re
 import requests
 import sys
 
@@ -10,44 +10,84 @@ import sys
 #
 
 headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"}
+arc = {}
 
-# download html file from given url
-try:
-   #response = requests.get('https://onepace.net/en/watch/', headers=headers)
-    #if response.status_code == 200:
-    with open("onepace.html", "r", encoding="utf-8") as file:
-        onepace = file.read()
-    htmlOutput = BeautifulSoup(onepace, "html.parser")
-    #else:
-    #   print("failed to get onepace.net")
-    #   sys.exit()
-except:
-    sys.exit("failed to download html from given url")
+def arcLinks():
+    # download html file from given url
+    response = requests.get('https://onepace.net/en/watch/', headers=headers)
+    if response.status_code == 200:
+    #with open("onepace.html", "r", encoding="utf-8") as file:
+    #    onepace = file.read()
+        htmlOutput = BeautifulSoup(response.text, "html.parser")
+    else:
+       sys.exit("failed to get onepace.net")
 
 #
 #   GRAB ARCS
 #
 
-arcParents = htmlOutput.find_all("li", id=True)
-arcs = {}
-# grab all the elements holding the arcs
-for arcLi in arcParents:
-    liID = arcLi['id']
-    # grab the elements in the arc that holds the list of all the links by dub/sub
-    for dubHolder in arcLi.find_all('li', attrs={'aria-labelledby': True}):
-        tempLinks = {}
-        # grab what says if links are dub, sub, or captions
-        dubSub = dubHolder.find('span', attrs={'class': 'flex-1'})
-        # find the list holding all the pixeldrain links
-        dubSubLinks = dubHolder.find('ul', role='list')
-        # create a new sub array for the overall links list
-        tempLinks.update({'dubSub': dubSub.get_text(), 'links': {}})
-        # go through the list holding the links and grab all the links
-        for links in dubSubLinks.find_all('a', href=True):
-            # get the resolution
-            resolution = links.find('span', attrs={'class': 'grow text-center whitespace-pre tabular-nums'}).get_text().replace('\u2007', '').strip()
-            # append the link and it's resolution to the temporary array
-            tempLinks['links'].update({resolution: links['href']})
-        # append the temp array to the full list of pixeldrain links for that arc
-    arcs.update({liID: tempLinks})
-print(arcs)
+    arcParents = htmlOutput.find_all("li", id=True)
+    # grab all the elements holding the arcs
+    for arcLi in arcParents:
+        liID = arcLi['id']
+        linksDict = {'languages': {}}
+        # grab the elements in the arc that holds the list of all the links by dub/sub
+        for dubHolder in arcLi.find_all('li', attrs={'aria-labelledby': True}):
+            dubSubLinksDict = {}
+            # grab what says if links are dub, sub, or captions
+            dubSub = dubHolder.find('span', attrs={'class': 'flex-1'})
+            # find the list holding all the pixeldrain links
+            dubSubLinks = dubHolder.find('ul', role='list')
+            # create a new sub array for the overall links list
+            dubSubLinksDict.update({})
+            # go through the list holding the links and grab all the links
+            for links in dubSubLinks.find_all('a', href=True):
+                # get the resolution
+                resolution = links.find('span', attrs={'class': 'grow text-center whitespace-pre tabular-nums'}).get_text().replace('\u2007', '').strip()
+                # append the link and it's resolution to the temporary array
+                dubSubLinksDict.update({resolution: links['href']})
+            # add the dubSub and links to language list
+            linksDict['languages'].update({dubSub.get_text().replace('\n                                    ', ' '): dubSubLinksDict})
+
+        arc.update({liID: linksDict})
+    with open ('links.json', 'w') as f:
+        f.write(json.dumps(arc, indent=4))
+
+arcLinks()
+
+#
+#   PIXELDRAIN
+#
+
+def pixelDrain():
+    arcDict = {}
+    # iterate arcs
+    for arcid, arcdata in arc.items():
+        # iterate languages
+        languages = arcdata['languages']
+        episodeDict = {}
+        for languageName, languageData in languages.items():
+            # iterate links
+            for linkRes, linkUrl in languageData.items():
+                # get pixeldrain api
+                langDict = {}
+                response = requests.get(linkUrl.replace('/l/', '/api/list/'), headers=headers)
+                if response.status_code != 200:
+                    sys.exit("failed to get pixeldrain api")
+                # get each episode and their id to get the api
+                for episode in response.json()['files']:
+                    episodeName = re.search(r'\[.*\] (.*) \[.*\]', episode['name']).group(1)
+                    langDict.update({
+                        episodeName: f'https://pixeldrain.net/api/file/{episode['id']}'
+                    })
+
+                if languageName not in episodeDict:
+                    episodeDict[languageName] = {}
+                episodeDict[languageName][linkRes] = langDict
+        arcDict.update({
+                arcid: episodeDict
+        })
+    with open ('linkspixeldrain.json', 'w') as f:
+        f.write(json.dumps(arcDict, indent=4))
+
+pixelDrain()
