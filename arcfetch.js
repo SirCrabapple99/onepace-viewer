33319983050dd145c0684b746e5fb4526d7e4838
SirCrabapple99 <99812880+SirCrabapple99@users.noreply.github.com>
2026-03-10 12:04:22 -0500

    setup episodes

--- arcfetch.js
+++ arcfetch.js
@@ -1,3 +1,18 @@
 async function parseArcsJSON() {
+  const response = await fetch('https://cdn.jsdelivr.net/gh/SirCrabapple99/onepace-viewer/linkspixeldrain.json')
+  const arcsJSON = await response.json()
+  for (const arc in arcsJSON) {
+    let option = document.createElement("option");
+    // get name in caps and stuff
+    const capsName = arc.split('-')
+        .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+        .join(' ')
+    option.innerHTML = capsName
+    option.value = arc
+    document.getElementById('arcDropDown').append(option)
+  }
+}
 
+window.onload = () => {
+  parseArcsJSON()
 }
